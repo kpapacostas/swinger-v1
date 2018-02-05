6eeba4e6b04dd062fea48ac8b5a637b8bfac375f
github username <github email address>
2018-02-05 09:26:58 -0500

    very few changes

--- swinger-backend/app/serializers/user_serializer.rb
+++ swinger-backend/app/serializers/user_serializer.rb
@@ -1,12 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :shows
 
-  def token(user_id)
-    issue_token(user_id)
-  end
-
-  def issue_token(payload)
-    JWT.encode(payload, 'secret', 'HS256')
-  end
 
 end
